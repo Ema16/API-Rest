59283f370c9f269dba2c9d002fae8e9bc9f5975f
Ema16 <eamperez89@gmail.com>
2021-05-13 21:09:16 -0600

    API Rest Examen Final Analisis y Diseño de Sistemas 2

---
+++ APIREST/Controllers/InventarioController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIREST.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InventarioController : ControllerBase
+    {
+        //EManuel Marlon Fabricio Amperez Tahuico - 201741942
+
+        //Operacion GET para consultar productos a la base de datos
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            //Aca estaria la coexion a la base de datos
+            return id switch
+            {
+                1 => "Producto",
+                2 => "Computadora ASUS",
+                _ => throw new NotSupportedException("El id no es valido")
+            };
+
+        }
+
+
+        //OPeracion POST para registrar productos a la base de datos
+        public string Post(Producto producto )
+        {
+            //Aca guardamos el producto en la base de datos
+
+            return producto.nombreProducto;
+        }
+
+        public class Producto
+        {
+            public string nombreProducto { get; set; }
+        }
+    }
+}
